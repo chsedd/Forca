b31ebec96971c20ae05f4a8d2225b33cd4a7b63a
chsedd <83226177+chsedd@users.noreply.github.com>
2025-03-30 14:37:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Forca ---");
+        
+        Console.Write("Qual a palavra secreta? ");
+        string palavraSecreta = Console.ReadLine().ToLower();
+        
+        Console.Write("Qual a letra? ");
+        char letra = Char.ToLower(Console.ReadKey().KeyChar);
+        
+        bool existe = palavraSecreta.Contains(letra);
+        
+        if (existe)
+        {
+            Console.WriteLine("\nA letra \"" + letra + "\" existe na palavra secreta => True");
+        }
+        else
+        {
+            Console.WriteLine("\nA letra \"" + letra + "\" não existe na palavra secreta => False");
+        }
+    }
+}
